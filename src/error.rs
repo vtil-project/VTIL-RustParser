cc8b6d9850578f65ac4e8f6a922bd503f9c78b5a
meme <meme@users.noreply.github.com>
2021-05-02 11:59:28 -0400

    Error handling overhaul

--- src/error.rs
+++ src/error.rs
@@ -30,62 +30,25 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-use std::fmt::{self, Display};
-use std::{error, io, str};
+use std::{io, str};
+use thiserror::Error;
 
-#[derive(Debug)]
 /// Custom `Error` for VTIL parsing
+#[derive(Error, Debug)]
 pub enum Error {
     /// An error occured during parsing due to a malformed VTIL file
+    #[error("Malformed VTIL file")]
     Malformed(String),
+
     /// An I/O error occured
-    Io(io::Error),
+    #[error("I/O error")]
+    Io(#[from] io::Error),
+
     /// Error inside of [Scroll](https://docs.rs/scroll) occured
-    Scroll(scroll::Error),
-}
+    #[error("Scroll error")]
+    Scroll(#[from] scroll::Error),
 
-impl error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::Malformed(_) => "Data is malformed",
-            Error::Io(_) => "I/O error",
-            Error::Scroll(_) => "Scroll error",
-        }
-    }
-
-    fn cause(&self) -> Option<&dyn error::Error> {
-        match *self {
-            Error::Malformed(_) => None,
-            Error::Io(ref err) => err.source(),
-            Error::Scroll(ref err) => err.source(),
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
-
-impl From<scroll::Error> for Error {
-    fn from(err: scroll::Error) -> Error {
-        Error::Scroll(err)
-    }
-}
-
-impl From<str::Utf8Error> for Error {
-    fn from(err: str::Utf8Error) -> Error {
-        Error::Malformed(err.to_string())
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Malformed(ref message) => write!(fmt, "Error while reading: {}", message),
-            Error::Io(ref err) => write!(fmt, "{}", err),
-            Error::Scroll(ref err) => write!(fmt, "{}", err),
-        }
-    }
+    /// Error during UTF-8 decoding, VTIL file is possibly malformed
+    #[error("UTF-8 decoding error")]
+    Utf8Error(#[from] str::Utf8Error)
 }
